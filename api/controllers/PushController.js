61c046e9de32813efcf428d854e8e3222ee0e6cf
Ruben Sebbane <ruben.sebbane@gmail.com>
2015-06-16 20:50:29 +0200

    schedule

---
+++ api/controllers/PushController.js
@@ -0,0 +1,13 @@
+/**
+ * PushController
+ *
+ * @description :: Server-side logic for managing pushes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+	
+	
+};
+
--- config/schedule.js
+++ config/schedule.js
@@ -2,6 +2,7 @@
  * Created by jaumard on 27/02/2015.
  */
  var async = require('async');
+ var moment = require('moment');
 
  module.exports.schedule = {
 
@@ -9,10 +10,8 @@
       //Everyday at 2:30AM
       "30 2 * * *"   : function ()
       {
-      	var nowMinus3d = new Date();
-      	nowMinus3d.setDate(nowMinus3d.getDate()-3);
-      	var nowMinus4d = new Date();
-      	nowMinus4d.setDate(nowMinus4d.getDate()-4);
+      	var nowMinus3d = moment().subtract(3, 'days').calendar();
+        var nowMinus4d = moment().subtract(4, 'days').calendar();
 
           // On sélectionne les gagnants des foots qui ont plus de 3 jours et qui ne sont terminés  		
           Vote.query("select max(nbVotes) as maxVotes, chevre, foot from (select count(*) as nbVotes, v.chevre, v.foot from vote v inner join foot f on f.id = v.foot group by v.chevre, v.foot where f.date <"+nowMinus3d+" and f.date > "+nowMinus4d+") x group by foot",function(err,results){
@@ -43,6 +42,7 @@
                   console.log(err);
                 Connexion.findOne({user:result.homme}).exec(function(err, connexion){
                   if(connexion){
+
                     sails.sockets.emit(connexion.socketId,'notif',actu);
                   }
                 });
@@ -58,11 +58,12 @@
 
       //Check end foot, trigger vote for each player
       //Every 4 hours
-      "* */4 * * *": function (){
-        var nowMinus4h = new Date();
-        nowMinus4h.setHours(nowMinus4h.getHours() - 4);
-        var nowMinus8h = new Date();
-        nowMinus8h.setHours(nowMinus8h.getHours() - 8);        
+      "0 */4 * * *": function (){
+        console.log("job test");
+        var nowMinus4h = moment().subtract(4, 'hours').calendar();
+
+        var nowMinus8h = moment().subtract(8, 'hours').calendar();
+
 // { date: { '>': nowMinus8h, '<': nowMinus4h }}
 Foot.find().exec(function(err, foots){
   async.each(foots, function(foot, callback){
@@ -89,6 +90,43 @@ Foot.find().exec(function(err, foots){
   });
 });
 
+},
+
+
+//SEND PUSH TO WARN USER ABOUT COMMING FOOT
+'*/10 * * * *' : function (){
+  var nowMinus3h10min = moment().subtract(3, 'hours').subtract(10, 'minutes').calendar();
+  var nowMinus3h = moment().subtract(3, 'hours').calendar();
+
+
+  Foot.find({ date: { '>': nowMinus3h10min, '<': nowMinus3h }}).exec(function(err, foots){
+
+    async.each(foots, function(foot, callback){
+      Player.find({foot:foot.id}).exec(function(err, players){
+        async.each(players, function(player, callback2){
+          Actu.create({user:player.user, related_user:player.user, typ:'3hoursBefore', related_stuff:foot.id}).exec(function(err,actu){
+            if(err)
+              console.log(err);
+            Connexion.findOne({user:player.user}).exec(function(err, connexion){
+              if(connexion){
+                sails.sockets.emit(connexion.socketId,'notif',actu);
+              }
+            });
+
+          });
+          callback2();
+        },function(err){
+
+        });
+
+      });
+      callback();
+    }, function(err){
+    });
+  });
+
+
 }
+
 
 };
