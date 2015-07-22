5cfd8b0e6fcf52550819c11455b736a54a4df811
0ruben <ruben.sebbane@gmail.com>
2015-07-22 19:55:35 +0200

    debug field ajout

--- api/services/ToolsService.js
+++ api/services/ToolsService.js
@@ -26,13 +26,10 @@ module.exports = {
       if(err)
         console.log(err)
       _.each(fields, function(field){
-        console.log(field);
-        field.cleanname = ToolsService.clean(field.name)+ToolsService.clean(field.city);
-        field.origin = 'public';
-        // field.createdAt = moment().format();
-        // field.updatedAt = moment().format();
-        field.partner = 0;
-        // field.save();
+        Field.update(field.id, {cleanname:ToolsService.clean(field.name)+ToolsService.clean(field.city), origin:'public',createdAt:moment().format(), updatedAt:moment().format(), partner:false})
+        .exec(function(err){
+            console.log(err);
+        });
       });
     });
   
