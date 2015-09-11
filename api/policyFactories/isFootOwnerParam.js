5a9f03f960da1ac6db5e25de772981045adafa22
0ruben <ruben.sebbane@gmail.com>
2015-09-11 12:41:30 +0200

    policy foot owner debug

---
+++ api/policyFactories/isFootOwnerParam.js
@@ -0,0 +1,43 @@
+module.exports = function(param){
+	return function(req, res, next){
+		var jwt = require('jsonwebtoken');   
+  	var auth = req.headers["authorization"];
+
+  	if(typeof auth !=='undefined'){                                              
+  		jwt.verify(auth,'123Tarbahh',function (err,decoded) {   
+  			if(err){
+  				return res.status(401).end();       
+  				next(); 
+  			} 
+  			if(decoded){
+  				Foot.find(req.param(param)).exec(function(err, foot){
+  					if(err){
+  						return res.status(401).end();      
+  						next();	
+  					}
+  					if(foot.created_by!=decoded.id){
+  						return res.status(401).end();
+  						next();       
+  					}
+  					else{
+  						next();
+  					}
+  				});
+  				
+  			}
+  			else{
+  				console.log("not autorized");
+  				return res.status(401).end();     
+  				next();  
+  			}
+
+  		});
+  	}
+
+  	else{ 
+  		console.log("not autorized");
+  		return res.status(401).end();
+  		next(); 
+  	}
+  }
+};
