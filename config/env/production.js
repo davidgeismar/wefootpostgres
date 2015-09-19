fc563c6235d50bd27f416952f364b1161ab5a2a9
Raphael Fettaya <raphaelfettaya@MacBook-Pro-de-mac.local>
2015-09-19 21:36:47 +0200

    Fix issues

--- config/env/production.js
+++ config/env/production.js
@@ -25,10 +25,10 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  // port: 80,
+  port: 80,
   models: {
-    connection: 'postgresTest',
-    migrate: 'alter'
+    connection: 'postgres',
+    migrate: 'safe'
   }
   // **************************************************************************
   //  * Set the port in the production environment to 80                        *
