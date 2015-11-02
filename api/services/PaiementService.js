3056388211aafee94fbd311855395bc93573b9a3
Raphael Fettaya <raphaelfettaya@MacBook-Pro-de-mac.local>
2015-11-03 00:07:35 +0200

    fix issue

--- api/services/PaiementService.js
+++ api/services/PaiementService.js
@@ -28,7 +28,7 @@ module.exports = {
         	else{
         		User.update({id: user.id},{mangoId: parseInt(wallet.Id)},function(err,user){
         			if(err) throw err;
-        			callback(user);
+        			callback(user[0]);
         		});
         	}
     	});
